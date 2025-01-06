1bd473eb8e5c9d8a71fd1aaf9e86763933e4deba
MaximDmytruk <dmitruk.max@gmail.com>
2025-01-07 00:20:20 +0200

    add structure of project

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Kitty',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(),
+      // routes: {
+      //   '/authorization_first': (_) => const AuthorizationFirst(),
+      // }
+      // initialRoute: ,
     );
   }
 }
