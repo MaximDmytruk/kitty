2fdbfee131d5c89b0e60bcdad61a46ffeb065a04
Maksym Dmytruk <dmitruk.max@gmail.com>
2025-01-19 18:12:57 +0200

    Add backgrondcolor in menu Dropdown

--- lib/widgets/custom_dropdown_menu.dart
+++ lib/widgets/custom_dropdown_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kitty/styles/colors/colors_app.dart';
 
 class CustomDropdownMenu extends StatefulWidget {
   final String selectedValue;
@@ -19,6 +20,11 @@ class _CustomDropdownMenuState extends State<CustomDropdownMenu> {
   @override
   Widget build(BuildContext context) {
     return DropdownMenu<String>(
+      menuStyle: MenuStyle(
+        backgroundColor: WidgetStatePropertyAll(
+          KittyColors.white,
+        ),
+      ),
       initialSelection: widget.selectedValue,
       expandedInsets: widget.padding,
       dropdownMenuEntries: [
