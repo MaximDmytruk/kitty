a6b5ca2e172f6b2b4209443f4eb9f534c14d00d0
Maksym Dmytruk <dmitruk.max@gmail.com>
2025-02-01 23:30:49 +0200

    Refactor: Add check in UserCubit

--- lib/cubit/user_cubit.dart
+++ lib/cubit/user_cubit.dart
@@ -15,8 +15,6 @@ part 'user_cubit.freezed.dart';
 class UserCubit extends Cubit<UserState> {
   UserCubit() : super(UserState());
 
-  // User? _currentUser;
-
   void registerUser(
     String password,
     String email,
@@ -29,8 +27,6 @@ class UserCubit extends Cubit<UserState> {
           status: UserStatus.error,
         ),
       );
-      print('USER is HERE');
-      // emit(UserState.error(AppLocale.errorUserRegistered));
     } else {
       User newUser = User(
         name: name,
@@ -43,8 +39,6 @@ class UserCubit extends Cubit<UserState> {
           status: UserStatus.authenticated,
         ),
       );
-
-      // emit(UserState.authenticated(_currentUser!));
     }
   }
 
@@ -56,10 +50,6 @@ class UserCubit extends Cubit<UserState> {
           status: UserStatus.error,
         ),
       );
-
-      // emit(
-      //   const UserState.error("No user registered"),
-      // );
       return;
     }
 
@@ -70,7 +60,6 @@ class UserCubit extends Cubit<UserState> {
           status: UserStatus.authenticated,
         ),
       );
-      // emit(UserState.authenticated(_currentUser!));
     } else {
       emit(
         state.copyWith(
@@ -78,22 +67,20 @@ class UserCubit extends Cubit<UserState> {
           status: UserStatus.error,
         ),
       );
-
-      // emit(const UserState.error("Invalid email or password"));
     }
   }
 
   void removeUser() {
     emit(
-      state.copyWith(user: null, status: UserStatus.initial),
+      state.copyWith(
+        user: null,
+        status: UserStatus.initial,
+      ),
     );
-    // _currentUser = null;
-    // emit(UserState.initial());
   }
 
   void addNewCategory(FinancialCategory newCategory) {
     state.user!.categoryService.addNewCategory(newCategory);
-
     showToast(text: 'New category added successfully!');
     emit(
       state.copyWith(
@@ -104,37 +91,25 @@ class UserCubit extends Cubit<UserState> {
   }
 
   void addFinancialTransaction(FinancialTransaction finTransaction) {
-    // emit(state.copyWith(listFinTransaction: [...state.listFinTransaction, finTransaction] ));
-
-    state.user!.finTransaction.add(finTransaction);
-    emit(
-      state.copyWith(user: state.user, status: UserStatus.authenticated),
-    );
-
-    // _currentUser!.finTransaction.add(finTransaction);
-    // emit(UserState.authenticated(_currentUser!));
+    if (state.user != null) {
+      state.user!.finTransaction.add(finTransaction);
+      emit(
+        state.copyWith(
+          user: state.user,
+          status: UserStatus.authenticated,
+        ),
+      );
+    } else {
+      emit(
+        state.copyWith(
+            status: UserStatus.error, errorText: 'No user registered'),
+      );
+    }
   }
 
-  String getUserName() {
-    // return _currentUser?.name.isNotEmpty == true
-    //     ? _currentUser!.name
-    //     : 'No Name';
-    return state.user!.name;
-  }
-
-  String getFirstLetterName() {
-    // return _currentUser?.name.isNotEmpty == true
-    //     ? _currentUser!.name[0].toUpperCase()
-    //     : '';
-    return state.user!.name[0].toUpperCase();
-  }
-
-  String getUserEmail() {
-    // return _currentUser?.email.isNotEmpty == true
-    //     ? _currentUser!.email
-    //     : 'No Email';
-    return state.user!.email;
-  }
+  String getUserName() => state.user?.name ?? 'No Name';
+  String getFirstLetterName() => state.user?.name[0].toUpperCase() ?? '-';
+  String getUserEmail() => state.user?.email ?? 'No Email';
 
   List<FinancialCategory> getFinancialCategory() {
     if (state.user != null) {
@@ -143,38 +118,58 @@ class UserCubit extends Cubit<UserState> {
 
       return categories;
     } else {
+      emit(
+        state.copyWith(
+          status: UserStatus.error,
+          errorText: '"No user registered"',
+        ),
+      );
       List<FinancialCategory> error = [];
+      return error;
+    }
+  }
+
+  List<FinancialTransaction> getFinancialTransaction() {
+    if (state.user != null) {
+      List<FinancialTransaction> transactions = state.user!.finTransaction;
+      return transactions;
+    } else {
+      emit(
+        state.copyWith(
+          status: UserStatus.error,
+          errorText: '"No user registered"',
+        ),
+      );
+      List<FinancialTransaction> error = [];
       return error;
     }
   }
 
   void showToast({required String text}) {
     BotToast.showCustomText(
-     
-        align: Alignment.topCenter,
-        toastBuilder: (_) {
-          return Padding(
-            padding: const EdgeInsets.only(top: 60.0),
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 8.0,
-              ),
-              color: ColorsApp.grey66,
-              child: Text(
-                text,
-                style: interTextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: ColorsApp.white,
-                ),
-                textAlign: TextAlign.center,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
+      align: Alignment.topCenter,
+      toastBuilder: (_) {
+        return Padding(
+          padding: const EdgeInsets.only(top: 60.0),
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
             ),
-          );
-        });
+            color: ColorsApp.grey66,
+            child: Text(
+              text,
+              style: interTextStyle(
+                fontWeight: FontWeight.w500,
+                color: ColorsApp.white,
+              ),
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        );
+      },
+    );
   }
-
- 
 }
