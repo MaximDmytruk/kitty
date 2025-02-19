1ac42ed99e1a542819c901eed403bb6d1b743867
Maksym Dmytruk <dmitruk.max@gmail.com>
2025-02-19 22:19:04 +0200

    Add methods for search in repositrry

--- lib/repositories/fin_transaction_repository/fin_transaction_repository.dart
+++ lib/repositories/fin_transaction_repository/fin_transaction_repository.dart
@@ -10,6 +10,18 @@ import 'package:sqflite/sqflite.dart';
 class FinTransactionRepository {
   final AppDatabase database = AppDatabase.instance;
 
+  //Some 
+  Future<List<FinancialTransaction>> searchTransactions(String query) async {
+    Database db = await database.database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'transactions',
+      where: 'description LIKE ? OR amount LIKE ?',
+      whereArgs: ['%$query%', '%$query%'],
+    );
+
+    return maps.map((map) => FinancialTransaction.fromJson(map)).toList();
+  }
+
   Future<int> getTotalAmount(
     int categoryId,
     int dateMonth,
